5b47837bd027cbf3c4846856d7e3488efb29c41d
Ubuntu <ubuntu@ip-172-31-57-171.ec2.internal>
2021-04-28 17:55:34 +0000

    added 2 files: area_per_rect.py & basic_math.py

---
+++ area_per_rect.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Created By: Layla Michel
+# Date: April 28, 2021
+# This program calculates the area and perimeter of a rectangle.
+
+def main():
+    print("If a rectangle has a length of 7 cm and width of 4cm ")
+    print("")
+    print("Area of the rectangle is {}cm^2.". format(7*4))
+    print("Perimeter of the rectangle is {}cm.". format(2*(7+4)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# Created By: Layla Michel
+# Date: April 28, 2021
+# This program does basic math.
+
+import math
+
+
+def main():
+    print("5 + 2 = {}". format(5+2))
+    print("3 - 7 = {}". format(3-7))
+    print("2 x 2 = {}". format(2*2))
+    print("7 / 4 = {}". format(7/4))
+    print("8 / 4 = {}". format(8//4))
+    print("8^2 = {}". format(8**2))
+    print("√25 = {}". format(math.sqrt(25)))
+
+
+if __name__ == "__main__":
+    main()
